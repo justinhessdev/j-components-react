abf81e5bd79280ab493fbf7a9a92b99fa06baff9
Administrator <admin@justins-mbp.lan>
2020-04-30 16:59:43 -0400

    module export

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import ReactDom from "react-dom";
 import { SearchDropdown } from "./src/lib/SearchDropdown/SearchDropdown";
 
-export { SearchDropdown };
+module.exports = SearchDropdown;

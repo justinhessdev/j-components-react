9c3edd9628ffaa53806e08357c21e19039e6e17a
Administrator <admin@justins-mbp.lan>
2020-04-30 18:25:59 -0400

    render

--- index.js
+++ index.js
@@ -2,4 +2,11 @@ import React from "react";
 import ReactDom from "react-dom";
 import { SearchDropdown } from "./src/lib/SearchDropdown/SearchDropdown";
 
+ReactDOM.render(
+  <React.StrictMode>
+    <div>Hello</div>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
+
 export { SearchDropdown };

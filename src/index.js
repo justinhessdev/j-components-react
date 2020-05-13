911067f548486817dd1ad3fb3bc8c42e5960d282
Administrator <admin@justins-mbp.lan>
2020-05-12 20:51:31 -0400

    remove example

--- src/index.js
+++ src/index.js
@@ -1,7 +1,1 @@
-import React from 'react'
-import styles from './styles.module.css'
 export { SearchDropdown } from './SearchDropdown/SearchDropdown'
-
-export const ExampleComponent = ({ text }) => {
-  return <div className={styles.test}>Example Component: {text}</div>
-}

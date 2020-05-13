c1e2f9205da2717036f8127d5fb6db55126a417e
Administrator <admin@justins-mbp.lan>
2020-05-12 20:55:03 -0400

    revert

--- src/index.tsx
+++ src/index.tsx
@@ -1,1 +1,10 @@
+import * as React from 'react'
 export { SearchDropdown } from './SearchDropdown/SearchDropdown'
+
+interface Props {
+  text: string
+}
+
+export const ExampleComponent = ({ text }: Props) => {
+  return <div>Example Component: {text}</div>
+}

39923f3ef9a64dd372c1e1c265dc1b70158fc866
Administrator <admin@justins-mbp.lan>
2020-05-23 12:13:27 -0400

    jest

--- src/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-export { SearchDropdown } from './SearchDropdown/SearchDropdown'
-
-export const ExampleComponent = ({ text }) => {
-  return <div>Example Component: {text}</div>
-}

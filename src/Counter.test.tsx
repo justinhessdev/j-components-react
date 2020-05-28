4b21e08e027f1612b155bd43313380d01e76425b
Administrator <admin@justins-mbp.lan>
2020-05-28 15:28:01 -0400

    snapshot test

--- src/Counter.test.tsx
+++ src/Counter.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { render, screen } from '@testing-library/react'
+import { render, screen, fireEvent } from '@testing-library/react'
 import Counter from './Counter'
 
 /**
@@ -7,7 +7,7 @@ import Counter from './Counter'
  */
 
 /**
- * Factoery function to create render for Coutner component
+ * Factory function to create render for Coutner component
  * @function setup
  * @param {object} props - component props sepecific to this setup
  * @param {any} state - Initial state for setup
@@ -32,13 +32,35 @@ test('renders counter display', () => {
   screen.getByTestId('counter-display') // errors out if there is no test id match
 })
 
-test('renders counter starts at 0', () => {})
+test('renders counter starts at 0', () => {
+  setup()
+  const counterDisplay = screen.getByTestId('counter-display')
+  expect(counterDisplay).toHaveTextContent('0')
+})
 
-test('renders clicking button increments counter display', () => {})
+test('renders clicking button increments counter display', () => {
+  setup()
+
+  // find counter display
+  const counterDisplay = screen.getByTestId('counter-display')
+
+  // initial display should be 0
+  expect(counterDisplay).toHaveTextContent('0')
+
+  // find increment button
+  const incrementButton = screen.getByTestId('increment-button')
+
+  // fire click event
+  fireEvent.click(incrementButton)
+
+  // display should be incremented by 1
+  expect(counterDisplay).toHaveTextContent('1')
+})
 
 test('renders diff snapshot clicking button increments counter', () => {
-  // const { getByText, asFragment } = render(<Counter />)
-  // const firstRender = asFragment()
-  // fireEvent.click(getByText(/Increment/))
-  // expect(firstRender).toMatchDiffSnapshot(asFragment())
+  const { asFragment } = setup()
+  const firstRender = asFragment()
+  const incrementButton = screen.getByTestId('increment-button')
+  fireEvent.click(incrementButton)
+  expect(firstRender).toMatchDiffSnapshot(asFragment())
 })
